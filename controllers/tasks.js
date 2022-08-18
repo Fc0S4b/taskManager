651d7915dce73cb5aeaa6cdaeea598f1da27a3ce
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-17 21:15:57 -0400

    create task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,14 @@
+const Task = require('../models/Task');
 const getAllTasks = (req, res) => {
   res.send('get all tasks');
 };
 
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+};
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
-};
-const createTask = (req, res) => {
-  res.json(req.body);
 };
 const updateTask = (req, res) => {
   res.send('update task');
