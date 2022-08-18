e46fded5d37ba4a0aa208901175257ceb2493186
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-17 21:43:11 -0400

    try/catch block

--- models/task.js
+++ models/task.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-// el objeto que tiene name es un validador, ya que si no lo tiene, entonces si se hacen solicitudes post vacías, se guardarán en la db y no son necesarias, es necesario un par de requisitos para que se guarde en el name ( y también un validador para completed). Mas info de validadores en los docs del paquete de mongoose
+
 const TaskSchema = new mongoose.Schema({
   name: {
     type: String,
     require: [true, 'must provide name'],
-    trim: true, //para eliminar espacios en blanco en extremos
+    trim: true,
     maxlength: [20, 'name can not be more than 20 characters'],
   },
   completed: {
