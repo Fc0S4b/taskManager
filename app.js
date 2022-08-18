b074412c2afca065feb683d63056f01c90914a7e
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-18 17:47:36 -0400

    custom error handler

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ require('dotenv').config();
 app.use(express.static('./public'));
 app.use(express.json());
 const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 // routes
 
 app.use('/api/v1/tasks', tasks);
 
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
