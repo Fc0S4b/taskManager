46244ebd46013cfdb6f800511b95a10db41a6dc7
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-18 16:36:50 -0400

    response types

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,7 +2,11 @@ const Task = require('../models/Task');
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
+    // res.status(200).json({ tasks });
+    // res.status(200).json({ tasks, amount: tasks.length });
+    res
+      .status(200)
+      .json({ status: 'success', data: { tasks, nbHits: tasks.length } });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
@@ -51,23 +55,6 @@ const updateTask = async (req, res) => {
     const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
       new: true,
       runValidators: true,
-      overwrite: true, //si quiero que se reescriba completamente todos los valores del put (completed y name), put hace esto
-    });
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id: ${taskID}` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
-const editTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
     });
     if (!task) {
       return res.status(404).json({ msg: `No task with id: ${taskID}` });
@@ -85,5 +72,4 @@ module.exports = {
   getTask,
   updateTask,
   deleteTask,
-  editTask,
 };
