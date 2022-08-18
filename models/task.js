97bfeec28e3199e68e69458d0301ae87230d3232
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-17 21:21:11 -0400

    schema info

--- models/task.js
+++ models/task.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// este es el único esquema válido, cualquier otra propiedad será ignorada (si pones en el body al hacer post en createTask en postman, otra propiedad que no sea name y completed, será ignorada)
 const TaskSchema = new mongoose.Schema({
   name: String,
   completed: Boolean
