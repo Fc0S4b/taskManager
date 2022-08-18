cd302f750fd43017cec55ff1b31f6ca0f9c63dbc
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-18 01:16:36 -0400

    delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -32,8 +32,23 @@ const getTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send('update task');
 };
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    // corrijo comentario anterior, si mantienes la estructura del id pero cambias un caracter, el id no existirá y arrojará error 404
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+    // otras alternativas
+    // res.status(200).send();
+    // res.status(200).json({ task: null, status: 'success' });
+
+    // si agregas o quitas un caracter, arrojará error 500 que es de mongoose
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
